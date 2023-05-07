19442f4121842b1bf63c8f34b9ea851f1eca9c42
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2023-05-07 10:01:19 -0500

    Migration UserTable created

--- Parcial3_BedoyaSamuel/Parcial3_BedoyaSamuel/Migrations/20230507145957_NewUserTable.Designer.cs
+++ Parcial3_BedoyaSamuel/Parcial3_BedoyaSamuel/Migrations/20230507145957_NewUserTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Parcial3_BedoyaSamuel.DAL;
 
@@ -11,9 +12,11 @@ using Parcial3_BedoyaSamuel.DAL;
 namespace Parcial3_BedoyaSamuel.Migrations
 {
     [DbContext(typeof(DataBaseContext))]
-    partial class DataBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230507145957_NewUserTable")]
+    partial class NewUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
